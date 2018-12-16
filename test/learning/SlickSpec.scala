3a734261d6da7310c24904a2b74cb0c05e1ef6b9
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-12-16 22:39:37 +0000

    DELETE ALL FROM A DATABASE

--- test/learning/SlickSpec.scala
+++ test/learning/SlickSpec.scala
@@ -17,22 +17,15 @@ import scala.concurrent.{Await, Future}
 
 class SlickSpec extends PlaySpec with GuiceOneAppPerTest with Injecting with MockitoSugar {
   "Slick" should {
-    "can select all" in {
+    "can delete all rows" in {
       var db = DatabaseConfigProvider.get[JdbcProfile]("mydb")(Play.current).db
-      assert(db.isInstanceOf[DatabaseDef])
-
       val userTable = TableQuery[UserTable]
-      val action = userTable.result
-
+      val action = userTable.delete
       val future = db.run(action)
-      assert(future.isInstanceOf[Future[User]])
-
-      val rows = Await.result(future, 2.seconds)
-      assert(rows.isInstanceOf[Vector[User]])
-      assert(rows.size === 1)
+      Await.result(future, 2.seconds)
     }
 
-    "can insert one row" in {
+    "can insert two rows" in {
       var db = DatabaseConfigProvider.get[JdbcProfile]("mydb")(Play.current).db
 
       val newusers = Seq(
@@ -49,8 +42,19 @@ class SlickSpec extends PlaySpec with GuiceOneAppPerTest with Injecting with Moc
       assert(rows == Some(2))
     }
 
-    "can delete all rows" in {
+    "can select all" in {
+      var db = DatabaseConfigProvider.get[JdbcProfile]("mydb")(Play.current).db
+      assert(db.isInstanceOf[DatabaseDef])
 
+      val userTable = TableQuery[UserTable]
+      val action = userTable.result
+
+      val future = db.run(action)
+      assert(future.isInstanceOf[Future[User]])
+
+      val rows = Await.result(future, 2.seconds)
+      assert(rows.isInstanceOf[Vector[User]])
+      assert(rows.size === 2)
     }
   }
 }
