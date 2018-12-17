fa6f8c17301d542bdb4e9ef078746c492ced474f
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-12-17 17:59:25 +0000

    added more

--- test/learning/Slick/SlickActionsSpec.scala
+++ test/learning/Slick/SlickActionsSpec.scala
@@ -12,22 +12,31 @@ import slick.jdbc.JdbcBackend._
 import slick.jdbc.JdbcProfile
 import slick.jdbc.MySQLProfile.api._
 import slick.lifted.TableQuery
-
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
 
-class SlickSpec extends PlaySpec with GuiceOneAppPerTest with Injecting with MockitoSugar {
+class SlickActionsSpec extends PlaySpec with GuiceOneAppPerTest with Injecting with MockitoSugar {
+  val userTable = TableQuery[UserTable]
 
   "Slick" should {
     "can delete all rows" in {
       var db = DatabaseConfigProvider.get[JdbcProfile]("mydb")(Play.current).db
-      val userTable = TableQuery[UserTable]
       val action = userTable.delete
       val future = db.run(action)
       Await.result(future, 2.seconds)
     }
 
-    "can insert two rows" in {
+    "can insert a simngle row" in {
+      var db = DatabaseConfigProvider.get[JdbcProfile]("mydb")(Play.current).db
+
+      val action = userTable += User(9, "eeeeee", "ffffff")
+      val future = db.run(action)
+      val rows = Await.result(future, 2.seconds)
+
+      assert(rows == 1)
+    }
+
+    "can insert two rows: Way 1" in {
       var db = DatabaseConfigProvider.get[JdbcProfile]("mydb")(Play.current).db
 
       val newusers = Seq(
@@ -35,9 +44,7 @@ class SlickSpec extends PlaySpec with GuiceOneAppPerTest with Injecting with Moc
         User(8, "cccccc", "ddddd")
       )
 
-      val userTable = TableQuery[UserTable]
       val action = userTable ++= newusers
-
       val future = db.run(action)
       val rows = Await.result(future, 2.seconds)
 
@@ -48,27 +55,22 @@ class SlickSpec extends PlaySpec with GuiceOneAppPerTest with Injecting with Moc
       var db = DatabaseConfigProvider.get[JdbcProfile]("mydb")(Play.current).db
       assert(db.isInstanceOf[DatabaseDef])
 
-      val userTable = TableQuery[UserTable]
       val action = userTable.result
-
       val future = db.run(action)
       assert(future.isInstanceOf[Future[User]])
 
       val rows = Await.result(future, 2.seconds)
       assert(rows.isInstanceOf[Vector[User]])
-      assert(rows.size === 2)
+      assert(rows.size === 3)
     }
 
-    "can get sql query string" in {
-      var db = DatabaseConfigProvider.get[JdbcProfile]("mydb")(Play.current).db
-      assert(db.isInstanceOf[DatabaseDef])
-
-      val userTable = TableQuery[UserTable]
-      val action = userTable.result
-
-      assert(List("select `id`, `first_name`, `last_name` from `user`") === action.statements)
-      assert(Some("select `id`, `first_name`, `last_name` from `user`") === action.statements.headOption)
-    }
+//    "can combine actions" in {
+//      val actionsCombined = (
+//        userTable.schema.create andThen
+//
+//
+//      )
+//    }
 
     "can  delete all" in  {
       var db = DatabaseConfigProvider.get[JdbcProfile]("mydb")(Play.current).db
---
+++ test/learning/Slick/SlickStatementsSpec.scala
@@ -0,0 +1,30 @@
+package learning.Slick
+
+import infrastructure.user.UserTable
+import org.scalatest.mockito.MockitoSugar
+import org.scalatestplus.play._
+import org.scalatestplus.play.guice._
+import play.api.test._
+import slick.jdbc.MySQLProfile.api._
+import slick.lifted.TableQuery
+
+class SlickStatementsSpec extends PlaySpec with GuiceOneAppPerTest with Injecting with MockitoSugar {
+  val userTable = TableQuery[UserTable]
+
+  "Slick" should {
+    "Statements represent the SQL query" in {
+      val statement1 = userTable.schema.createStatements
+      assert(statement1.isInstanceOf[Iterator[Unit]])
+
+      val statement2 = userTable.schema.createStatements.mkString
+      assert("create table `user` (`id` BIGINT NOT NULL PRIMARY KEY,`first_name` TEXT NOT NULL,`last_name` TEXT NOT NULL)" === statement2)
+
+      val action1 = userTable.result
+      assert(List("select `id`, `first_name`, `last_name` from `user`") === action1.statements)
+      assert(Some("select `id`, `first_name`, `last_name` from `user`") === action1.statements.headOption)
+
+      val action2 = userTable.schema.create
+      assert(action2.isInstanceOf[DBIO[Unit]])
+    }
+  }
+}
