2af325d8fdc914b3bd07e6fa2aafb10213151fec
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-01-05 15:55:40 +0000

    added plain sql painful as shit

--- test/learning/Slick/RunPlainSqlSpec.scala
+++ test/learning/Slick/RunPlainSqlSpec.scala
@@ -7,7 +7,7 @@ import org.scalatestplus.play.guice.GuiceOneAppPerTest
 import play.api.Play
 import play.api.db.slick.DatabaseConfigProvider
 import play.api.test.Injecting
-import slick.jdbc.JdbcProfile
+import slick.jdbc.{GetResult, JdbcProfile}
 import slick.lifted.TableQuery
 
 import scala.concurrent.duration._
@@ -17,15 +17,33 @@ import slick.jdbc.MySQLProfile.api._
 class RunPlainSqlSpec extends FlatSpec with GuiceOneAppPerTest with Injecting {
   val userTable = TableQuery[UserTable]
 
+
   "Slick" should "run plain sql queries" in {
     val db = cleanDb()
     givenDbFixture(db)
 
-    val action = sql""" select first_name from user """.as[String]
-
+    var action = sql"""select first_name from user""".as[String]
     var results = Await.result(db.run(action), 2.seconds)
     assert(results === Vector("aaaaaa", "cccccc"))
+
+
+    var action1 = sql"""select * from user""".as[(Long, String, String)]
+    var results1 = Await.result(db.run(action1), 2.seconds)
+    assert(results1 === Vector((7,"aaaaaa","bbbbb"), (8,"cccccc","ddddd")))
+
+
+    var action3 = sql"""select * from user""".as[(Long, String)]
+    var results3 = Await.result(db.run(action3), 2.seconds)
+    assert(results3 === Vector((7, "aaaaaa"), (8, "cccccc")))
+
+    
+    implicit val getChannelResult = GetResult(r => User(r.<<, r.<<, r.<<))
+    var action2 = sql"""select * from user""".as
+    var results2 = Await.result(db.run(action2), 2.seconds)
+    assert(results2 === Vector(User(7,"aaaaaa","bbbbb"), User(8,"cccccc","ddddd")))
   }
+
+
 
   private def cleanDb(): JdbcProfile#Backend#Database = {
     var db = DatabaseConfigProvider.get[JdbcProfile]("mydb")(Play.current).db
