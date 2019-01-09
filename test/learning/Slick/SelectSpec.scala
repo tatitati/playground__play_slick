fe60989c7f533b1dee47a86be3159b90176bec70
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-01-09 20:48:55 +0000

    add select LIMIT

--- test/learning/Slick/SelectSpec.scala
+++ test/learning/Slick/SelectSpec.scala
@@ -2,6 +2,7 @@ package learning.Slick
 
 import App.Domain.User
 import infrastructure.user.UserSchema
+import org.scalatest.FunSuite
 import org.scalatest.mockito.MockitoSugar
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
@@ -16,65 +17,79 @@ import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
 
 
-class SelectSpec extends PlaySpec with GuiceOneAppPerTest with Injecting with MockitoSugar {
+class SelectSpec extends FunSuite with GuiceOneAppPerTest with Injecting with MockitoSugar {
   val userTable = TableQuery[UserSchema]
 
-  "Slick" should {
-    "can select all rows" in {
-      var db = DatabaseConfigProvider.get[JdbcProfile]("mydb")(Play.current).db
-      exec(
-          userTable.delete andThen
-          (userTable ++= Seq(User(7, "aaaaaa", "bbbbb"), User(8, "cccccc", "ddddd"))),
-        db
-      )
 
-      val rows = exec(userTable.result, db)
-
-      assert(rows.isInstanceOf[Vector[User]])
-      assert(rows.size === 2)
-    }
-
-    "can select one column" in {
-      var db = DatabaseConfigProvider.get[JdbcProfile]("mydb")(Play.current).db
-      exec(
-          userTable.delete andThen
-          (userTable ++= Seq(User(7, "aaaaaa", "bbbbb"), User(8, "cccccc", "ddddd"))),
-        db
-      )
-
-      val rows = exec(userTable.map(_.firstName).result, db)
-
-      assert(rows === Vector("aaaaaa", "cccccc"))
-    }
-
-    "can select multiple (but not all) columns" in {
-      var db = DatabaseConfigProvider.get[JdbcProfile]("mydb")(Play.current).db
-      exec(
+  test("can select all rows") {
+    var db = DatabaseConfigProvider.get[JdbcProfile]("mydb")(Play.current).db
+    exec(
         userTable.delete andThen
-          (userTable ++= Seq(User(7, "aaaaaa", "bbbbb"), User(8, "cccccc", "ddddd"))),
-        db
-      )
+        (userTable ++= Seq(User(7, "aaaaaa", "bbbbb"), User(8, "cccccc", "ddddd"))),
+      db
+    )
 
-      val rows = exec(userTable.map(t => (t.firstName, t.id)).result, db)
+    val rows = exec(userTable.result, db)
 
-      assert(rows === Vector(("aaaaaa", 7),("cccccc", 8)))
-    }
-
-    "can select all_types_studio" in {
-      var db = DatabaseConfigProvider.get[JdbcProfile]("mydb")(Play.current).db
-      exec(
-          userTable.delete andThen
-          (userTable ++= Seq(User(7, "aaaaaa", "bbbbb"), User(8, "cccccc", "ddddd"))),
-        db
-      )
-
-      val future = db.run(userTable.result)
-      assert(future.isInstanceOf[Future[User]])
-
-      val rows = Await.result(future, 2.seconds)
-      assert(rows.isInstanceOf[Vector[User]])
-    }
+    assert(rows.isInstanceOf[Vector[User]])
+    assert(rows.size === 2)
   }
+
+  test("can select one column") {
+    var db = DatabaseConfigProvider.get[JdbcProfile]("mydb")(Play.current).db
+    exec(
+        userTable.delete andThen
+        (userTable ++= Seq(User(7, "aaaaaa", "bbbbb"), User(8, "cccccc", "ddddd"))),
+      db
+    )
+
+    val rows = exec(userTable.map(_.firstName).result, db)
+
+    assert(rows === Vector("aaaaaa", "cccccc"))
+  }
+
+  test("can select multiple (but not all) columns") {
+    var db = DatabaseConfigProvider.get[JdbcProfile]("mydb")(Play.current).db
+    exec(
+      userTable.delete andThen
+        (userTable ++= Seq(User(7, "aaaaaa", "bbbbb"), User(8, "cccccc", "ddddd"))),
+      db
+    )
+
+    val rows = exec(userTable.map(t => (t.firstName, t.id)).result, db)
+
+    assert(rows === Vector(("aaaaaa", 7),("cccccc", 8)))
+  }
+
+  test("can use LIMIT when selecting") {
+    var db = DatabaseConfigProvider.get[JdbcProfile]("mydb")(Play.current).db
+    exec(
+      userTable.delete andThen
+        (userTable ++= Seq(User(7, "aaaaaa", "bbbbb"), User(8, "cccccc", "ddddd"))),
+      db
+    )
+
+    val rows = exec(userTable.take(1).result, db)
+
+    assert(rows.isInstanceOf[Vector[User]])
+    assert(rows.size === 1)
+  }
+
+  test("can select all_types_studio") {
+    var db = DatabaseConfigProvider.get[JdbcProfile]("mydb")(Play.current).db
+    exec(
+        userTable.delete andThen
+        (userTable ++= Seq(User(7, "aaaaaa", "bbbbb"), User(8, "cccccc", "ddddd"))),
+      db
+    )
+
+    val future = db.run(userTable.result)
+    assert(future.isInstanceOf[Future[User]])
+
+    val rows = Await.result(future, 2.seconds)
+    assert(rows.isInstanceOf[Vector[User]])
+  }
+
 
   private def exec[T](action: DBIO[T], db: JdbcProfile#Backend#Database): T =
   {
