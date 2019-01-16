aaed876941e8d1ace645cf887d538e0f3eafe682
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-01-16 22:56:42 +0000

    asdasdfadsf

--- test/learning/Slick/LeftJoinSpec.scala
+++ test/learning/Slick/LeftJoinSpec.scala
@@ -16,14 +16,12 @@ import scala.concurrent.duration._
 import scala.concurrent.Await
 
 
-
 class LeftJoinSpec extends FunSuite with GuiceOneAppPerTest with Injecting with MockitoSugar with Exec {
   val writerTable = TableQuery[WriterSchema]
   val userTable = TableQuery[UserSchema]
   val messageTable = TableQuery[MessageSchema]
 
-
-  test("LEFT JOIN: create fixture with two writers") {
+  test("Create fixture with two writers") {
     implicit val db = DatabaseConfigProvider.get[JdbcProfile]("mydb")(Play.current).db
     exec(
       messageTable.schema.drop andThen
@@ -40,7 +38,7 @@ class LeftJoinSpec extends FunSuite with GuiceOneAppPerTest with Injecting with 
     assert(writerId2 === Vector(2))
   }
 
-  test("LEFT JOIN: create fixture of two messages referencing one of the writer") {
+  test("Create fixture of two messages referencing one of the writer") {
     implicit val db = DatabaseConfigProvider.get[JdbcProfile]("mydb")(Play.current).db
 
     var messageId1 = exec(MessageDao.insertMessage ++= Seq(Message(2, "messageContent1")))
@@ -50,7 +48,7 @@ class LeftJoinSpec extends FunSuite with GuiceOneAppPerTest with Injecting with 
     assert(messageId2 === Vector(2))
   }
 
-  test("LEFT JOIN: request a message (root) with a writer relationship") {
+  test("Request a message (root) with a writer relationship") {
     implicit val db = DatabaseConfigProvider.get[JdbcProfile]("mydb")(Play.current).db
 
     var future = db.run(messageTable.joinLeft(writerTable).on(_.senderId === _.id).result)
@@ -58,14 +56,14 @@ class LeftJoinSpec extends FunSuite with GuiceOneAppPerTest with Injecting with 
 
     assert(
       message == Vector(
-      (
-        Message(2,"messageContent1",1),
-        Some(Writer("writer2","writersurname2",2))
-      ), (
-        Message(2,"messageContent2",2),
-        Some(Writer("writer2","writersurname2",2)))
+        (
+          Message(2,"messageContent1",1),
+          Some(Writer("writer2","writersurname2",2))
+        ), (
+          Message(2,"messageContent2",2),
+          Some(Writer("writer2","writersurname2",2))
+        )
       )
     )
   }
-
 }
