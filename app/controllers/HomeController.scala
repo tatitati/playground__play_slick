1ccb6efeac0ef26fb5490dd4c5d28a16fa26da43
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-01-21 19:21:34 +0000

    added boxfuse deployment params (in a very fucking shit way)

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -73,14 +73,7 @@ class HomeController @Inject() (
         Ok("done")
     }
 
-    /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
-  }
+    def index() = Action { implicit request: Request[AnyContent] =>
+        Ok(views.html.index())
+    }
 }
